fbcb7aff44967a93a956bb7c960b42d6533d280c
tuongmartin <tuongbkav@gmail.com>
2017-07-11 09:35:49 +0700

    Adjust EmployeeDAO by Martin

--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -16,12 +16,12 @@ public class EmployeeDAO {
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public List<Employee> getList() {
-		String sql = "Select employee.id,employee.name,birthday,address,phone,picture,role.namePos as namePos,team.name as nameTeam from employee left join role on employee.id_Position = role.id left join team on employee.id_Team = team.id";
+		String sql = "Select Employee.id,Employee.name,birthday,address,phone,picture,Role.namePos as namePos,Team.name as nameTeam from Employee left join Role on Employee.id_Position = Role.id left join Team on Employee.id_Team = Team.id";
 		return jdbcTemplate.query(sql, new BeanPropertyRowMapper(Employee.class));
 	}
 
 	public int addItem(Employee objItem) {
-		String sql = "Insert into employee(id,name,birthday,address,phone,picture,id_Position,id_Team) values(?,?,?,?,?,?,?,?)";
+		String sql = "Insert into Employee(id,name,birthday,address,phone,picture,id_Position,id_Team) values(?,?,?,?,?,?,?,?)";
 		return jdbcTemplate.update(sql,
 				new Object[] { objItem.getId(), objItem.getName(), objItem.getBirthday(), objItem.getAddress(),
 						objItem.getPhone(), objItem.getPicture(), objItem.getId_Position(), objItem.getId_Team() });
@@ -29,20 +29,20 @@ public class EmployeeDAO {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Employee getItem(String idEmployee) {
-		String sql = "Select * from employee where id = ?";
+		String sql = "Select * from Employee where id = ?";
 		return (Employee) jdbcTemplate.queryForObject(sql, new Object[] { idEmployee },
 				new BeanPropertyRowMapper(Employee.class));
 	}
 
 	public int delItem(String id) {
-		String sql = "Delete from employee where id = ?";
+		String sql = "Delete from Employee where id = ?";
 		return jdbcTemplate.update(sql, new Object[] { id });
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public boolean checkID(String id) {
 		boolean check = true;
-		String query = "SELECT * FROM employee";
+		String query = "SELECT * FROM Employee";
 		List<Employee> objItem = jdbcTemplate.query(query, new BeanPropertyRowMapper(Employee.class));
 		for (Employee item : objItem) {
 			if (id.equalsIgnoreCase(item.getId())) {
@@ -55,7 +55,7 @@ public class EmployeeDAO {
 
 	public int editItem(Employee objEmployee) {
 
-		String sql = "UPDATE employee SET name = ?, birthday = ?, address = ?, phone = ?, picture = ?, id_Position = ?, id_Team = ? WHERE id = ?";
+		String sql = "UPDATE Employee SET name = ?, birthday = ?, address = ?, phone = ?, picture = ?, id_Position = ?, id_Team = ? WHERE id = ?";
 		return jdbcTemplate.update(sql,
 				new Object[] { objEmployee.getName(), objEmployee.getBirthday(), objEmployee.getAddress(),
 						objEmployee.getPhone(), objEmployee.getPicture(), objEmployee.getId_Position(),
